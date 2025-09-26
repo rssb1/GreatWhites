3baa01596fe1ea5ed055d2cf174a42873cfb1e22
gcolonro-bash <gcolonro@charlotte.edu>
2025-09-26 12:09:28 -0400

    Add files via upload

--- Step_1/statesharkattacks.sh
+++ Step_1/statesharkattacks.sh
@@ -2,33 +2,83 @@
 
 # --- Data Storage ---
 # We use an associative array to store the shark attack data.
-# This is a much better way to handle many states than using individual variables.
+# Note that keys like "South Carolina" now include spaces.
 declare -A shark_attacks
 
 # Populate the array with state names and their shark attack counts.
-# The number 0 signifies no attacks, which we'll handle below.
+shark_attacks[Alabama]=21
+shark_attacks[Alaska]=5
+shark_attacks[Arizona]=2
+shark_attacks[Arkansas]=1
+shark_attacks[California]=0
+shark_attacks[Colorado]=0
+shark_attacks[Connecticut]=0
+shark_attacks[Delaware]=0
 shark_attacks[Florida]=21
-shark_attacks[Hawaii]=5
-shark_attacks[California]=2
-shark_attacks[SouthCarolina]=1
-shark_attacks[Texas]=0
-shark_attacks[Tennessee]=0
-shark_attacks[Ohio]=0
-shark_attacks[Pennsylvania]=0
+shark_attacks[Georgia]=5
+shark_attacks[Hawaii]=2 
+shark_attacks[Idaho]=1
+shark_attacks[Illinois]=0
+shark_attacks[Indiana]=0
+shark_attacks[Iowa]=0
+shark_attacks[Kansas]=0
+shark_attacks[Kentucky]=21
+shark_attacks[Louisiana]=5
+shark_attacks[Maine]=2
+shark_attacks[Maryland]=1
+shark_attacks[Massachusetts]=0
+shark_attacks[Michigan]=0
+shark_attacks[Minnesota]=0
+shark_attacks[Mississippi]=0
+shark_attacks[Missouri]=21
+shark_attacks[Montana]=5 
+
+
+shark_attacks[Nebraska]=2
+shark_attacks[Nevada]=1
+shark_attacks[New\ Hampshire]=0
+shark_attacks[New\ Jersey]=0
+shark_attacks[New\ Mexico]=0
+shark_attacks[New\ York]=0
+shark_attacks[North\ Carolina]=21
+shark_attacks[North\ Dakota]=5
+shark_attacks[Ohio]=2
+shark_attacks[Oklahoma]=1
+shark_attacks[Oregon]=0
+shark_attacks[Pennslyvania]=0
+shark_attacks[Rohode\ Island]=0
+shark_attacks[South\ Carolina]=0
+shark_attacks[South\ Dakota]=21
+shark_attacks[Tennessee]=5
+shark_attacks[Texas]=2
+shark_attacks[Utah]=1
+shark_attacks[Vermont]=0
+shark_attacks[Virginia]=0
+shark_attacks[Washington]=0
+shark_attacks[West Virginia]=0
+shark_attacks[Wisconsin]=21
+shark_attacks[Wyoming]=5 
+
+
+shark_attacks[District\ of\ Columbia]=2
+shark_attacks[South\ Carolina]=1
+shark_attacks[American\ Samoa]=0
+shark_attacks[Guam]=0
+shark_attacks[North\ Mariana\ Islands]=0
+shark_attacks[Puerto\ Rico]=0
+shark_attacks[U.S.\ Virgin\ Islands]=21
 
 # --- User Input ---
 # Prompt the user for a state name and store the input.
 read -p "Please enter a state name to check for shark attacks: " state_input
 
-# Convert the user's input to Title Case.
-# This ensures that "florida," "FLORIDA," or "Florida" will all match the key in our array.
-# The 'sed' command with 's/\(.\)/\U\1/g' capitalizes the first letter of each word.
-# The 'tr' command converts the rest of the string to lowercase.
-# We then combine them to get the desired title case.
-formatted_input=$(echo "$state_input" | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));}1')
-
 # --- Logic ---
-# First, check if the state name entered by the user exists as a key in our array.
+# First, we format the user's input to match the keys in our array.
+# The 'sed' command capitalizes the first letter of each word.
+formatted_input=$(echo "$state_input" | sed 's/\b[a-z]/\U&/g')
+
+# Now, we check if the formatted state name exists as a key in our array.
+# We use double brackets [[...]] for the check and quote the variable.
 if [[ -v shark_attacks["$formatted_input"] ]]; then
     # If the state exists, get the number of attacks.
     count=${shark_attacks["$formatted_input"]}
@@ -45,7 +95,7 @@ if [[ -v shark_attacks["$formatted_input"] ]]; then
         case "$see_locations" in
             [Yy]* )
                 echo "Displaying the locations of the shark attacks..."
-                echo "..." # You can list specific locations here if you have them.
+                # You can list specific locations here if you have them.
                 ;;
             [Nn]* )
                 echo "Okay, no problem."
